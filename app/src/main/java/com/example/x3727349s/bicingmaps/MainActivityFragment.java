02a4d86ab136ce18818caba8a47712607b99ec56
Taha <x3727349s@iespoblenou.org>
2017-02-13 19:18:58 +0100

    Tenemos el zoom y initialize Map echos y funcionando

--- app/src/main/java/com/example/x3727349s/bicingmaps/MainActivityFragment.java
+++ app/src/main/java/com/example/x3727349s/bicingmaps/MainActivityFragment.java
@@ -2,11 +2,20 @@ package com.example.x3727349s.bicingmaps;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,6 +23,11 @@ import org.osmdroid.views.MapView;
 public class MainActivityFragment extends Fragment {
 
     private MapView map;
+    private MyLocationNewOverlay myLocationOverlay;
+    private MinimapOverlay mMinimapOverlay;
+    private ScaleBarOverlay mScaleBarOverlay;
+    private CompassOverlay mCompassOverlay;
+    private IMapController mapController;
 
     public MainActivityFragment() {
     }
@@ -24,12 +38,75 @@ public class MainActivityFragment extends Fragment {
         View view =inflater.inflate(R.layout.fragment_main, container, false);
 
         map = (MapView) view.findViewById(R.id.mapView);
+        
+        initializeMap();
+        setZoom();
+        setOverlays();
+        
+        map.invalidate();
+
+
+        return view;
+    }
+
+    private void setOverlays() {
+
+      /*  final DisplayMetrics dm = getResources().getDisplayMetrics();
+
+        myLocationOverlay = new MyLocationNewOverlay(getContext(),new GpsMyLocationProvider(getContext()),map);
+        myLocationOverlay.enableMyLocation();
+        myLocationOverlay.runOnFirstFix(new Runnable() {
+            public void run() {
+                mapController.animateTo( myLocationOverlay.getMyLocation());
+            }
+        });*/
+
+/*
+        mMinimapOverlay = new MinimapOverlay(getContext(), map.getTileRequestCompleteHandler());
+        mMinimapOverlay.setWidth(dm.widthPixels / 5);
+        mMinimapOverlay.setHeight(dm.heightPixels / 5);
+*/
+
+      /*  mScaleBarOverlay = new ScaleBarOverlay(map);
+        mScaleBarOverlay.setCentred(true);
+        mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+
+        mCompassOverlay = new CompassOverlay(
+                getContext(),new InternalCompassOrientationProvider(getContext()),map);
+        mCompassOverlay.enableCompass();
+
+        map.getOverlays().add(myLocationOverlay);
+        map.getOverlays().add(this.mMinimapOverlay);
+        map.getOverlays().add(this.mScaleBarOverlay);
+        map.getOverlays().add(this.mCompassOverlay);*/
+
+    }
+
+    private void setZoom() {
+        //  Setteamos el zoom al mismo nivel y ajustamos la posición a un geopunto
+        mapController = map.getController();
+        mapController.setZoom(15);
+    }
+
+    private void initializeMap() {
+
         map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
         map.setTilesScaledToDpi(true);
 
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
-
-        return view;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
